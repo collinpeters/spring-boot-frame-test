152a7b720022c649482583af7328bfbc733cf2bf
Rob Winch <rwinch@gopivotal.com>
2016-03-11 15:24:33 -0600

    Make XFrames Same Origin

--- frame-test-resource/src/main/java/test/ResourceServer.java
+++ frame-test-resource/src/main/java/test/ResourceServer.java
@@ -3,13 +3,16 @@ package test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @EnableAutoConfiguration
-@EnableResourceServer
 @ComponentScan
 public class ResourceServer {
 
@@ -22,4 +25,18 @@ public class ResourceServer {
 		return "test";
 	}
 
+	@Configuration
+	static class MyResourceServerConfigurerAdapter extends ResourceServerConfiguration {
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			super.configure(http);
+			http
+				.headers()
+					.frameOptions()
+						.sameOrigin();
+
+		}
+
+	}
 }
